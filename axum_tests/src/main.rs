c83792d1e77c1f1cd55dbb3a4fce07e0f9174d0d
Azamat <hunter0315277@gmail.com>
2026-04-27 19:10:07 +0500

    feat: modularize routes, add custom error module, and implement static file serving

--- axum_tests/src/main.rs
+++ axum_tests/src/main.rs
@@ -6,24 +6,40 @@ use axum::extract::Query;
 use axum::response::Html;
 use axum::response::IntoResponse;
 use axum::routing::get;
+use axum::routing::get_service;
 use serde::Deserialize;
+use tower_http::services::ServeDir;
 use std::net::SocketAddr;
+
+mod error;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let routes_hello = Router::new()
-        .route("/hello", get(handler_hello))
-        .route("/hello2/:name", get(handler_hello2));
+    let routes_all = Router::new()
+    .merge(routes_hello())
+    .fallback_service(routes_static());
 
     // region: -- Start Server
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     println!("->> LISTENING on {addr}\n");
     let listener = tokio::net::TcpListener::bind(addr).await?;
-    axum::serve(listener, routes_hello).await?;
+    axum::serve(listener, routes_all).await?;
     // endregion: -- Start Server
-
+    
     Ok(())
 }
+
+fn routes_hello() -> Router {
+    Router::new()
+    .route("/hello", get(handler_hello))
+    .route("/hello2", get(handler_hello2))
+}
+
+fn routes_static() -> ServeDir {
+    ServeDir::new("./")
+}
+
+
 
 #[derive(Debug, Deserialize)]
 struct HelloParams {
